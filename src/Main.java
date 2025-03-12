714045fec355fcbec829e4af280a78c0f5b0da93
Dmitri Nedioglo <dm.nedioglo@gmail.com>
2025-03-12 21:54:25 +0100

    Extras по желанию

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +15,25 @@ public class Main {
         mailDeliveryServiceList.add(pigeonService);
 
         sendMailAll(mailDeliveryServiceList);
+        System.out.println("--------------------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите номер способа доставки:");
+        System.out.println("1. DHL");
+        System.out.println("2. Email");
+        System.out.println("3. Почтовый голубь");
+        String dhl = "1";
+        String email = "2";
+        String pigeon = "3";
+        String result = scanner.nextLine();
+        if (result.equals(dhl)) {
+            System.out.println("Ок! Вы выбрали способ доставки: " + dhlService.serviceTitle());
+        }
+        if (result.equals(email)) {
+            System.out.println("Ок! Вы выбрали способ доставки: " + emailService.serviceTitle());
+        }
+        if (result.equals(pigeon)) {
+            System.out.println("Ок! Вы выбрали способ доставки: " + pigeonService.serviceTitle());
+        }
 
 
     }
@@ -23,4 +43,5 @@ public class Main {
             mailDeliveryService.sendMail();
         }
     }
+
 }
